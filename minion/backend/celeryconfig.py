95c3b12234d2da094026583bc47417cb80722cd1
Ilya Glotov <ilya.glotov@gmail.com>
2017-03-28 14:17:03 +0300

    Use mongo specified in config

--- minion/backend/celeryconfig.py
+++ minion/backend/celeryconfig.py
@@ -1,4 +1,9 @@
+from minion.backend.utils import backend_config
+
+cfg = backend_config()
+
 CELERY_ACCEPT_CONTENT = ["json", "pickle"]
 CELERY_MONGODB_SCHEDULER_DB = "minion"
 CELERY_MONGODB_SCHEDULER_COLLECTION = "scanschedule"
-CELERY_MONGODB_SCHEDULER_URL = "mongodb://localhost"
+CELERY_MONGODB_SCHEDULER_URL = "mongodb://{}:{}".format(cfg['mongodb']['host'],
+                                                        cfg['mongodb']['port'])
